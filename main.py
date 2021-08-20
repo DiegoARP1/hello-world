f83062760d3bf5fb0592767ecb953425baadda17
JZapataMA <juanmazm9@gmail.com>
2021-08-20 15:23:05 -0400

    DIEGO JOLAPERRA SACATE UN MAMBO EN EL BELLA QUIEEEEE

---
+++ main.py
@@ -0,0 +1,6 @@
+#truco para hacer tableros
+p = [['' for x in range(#colum)] for x in range(#filas)]
+print(p)
+
+lista = []
+listanueva = sorted(lista, key = lambda x: (#eelementos que quiero tomar de la lista para compararlas de pirmero en orden))
